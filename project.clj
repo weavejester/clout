9dd21bbfa7ffc18cb045a9f3a7f9edae19331254
weavejester <jreeves@weavejester.com>
2010-01-16 17:35:29 +0000

    Added Clojars dependency to project file

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clout "0.1"
   :description "A HTTP route matching library"
   :url "http://github.com/weavejester/clout"
-  :dependencies [[org.clojure/clojure "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.1.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
