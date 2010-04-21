9c9a7632779f0f8e04a46d41b33d68245ccfc02c
weavejester <jreeves@weavejester.com>
2010-04-21 01:57:11 +0100

    Removed SNAPSHOT from lein-clojars dependency

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A HTTP route matching library"
   :url "http://github.com/weavejester/clout"
   :dependencies [[org.clojure/clojure "1.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+  :dev-dependencies [[lein-clojars "0.5.0"]])
