d5d7556ac742d323c75fb3cdd82d4685fb06646a
weavejester <jreeves@weavejester.com>
2010-11-29 21:07:31 +0000

    Removed unnecessary dev dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject clout "0.3.1"
   :description "A HTTP route matching library"
   :url "http://github.com/weavejester/clout"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]
-                     [swank-clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]])
