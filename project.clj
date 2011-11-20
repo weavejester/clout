aa6f15602b714420186ee278ce99473eafadb11e
James Reeves <jreeves@weavejester.com>
2011-11-20 23:15:51 +0000

    Updated Clojure version to 1.2.1 (or above)

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clout "0.4.1"
   :description "A HTTP route matching library"
   :url "http://github.com/weavejester/clout"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[ring-mock "0.1.1"]])
