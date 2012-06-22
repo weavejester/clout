f51b8e3d671f4167dbb7bc2809488f8ebc025304
James Reeves <jreeves@weavejester.com>
2012-06-22 19:17:43 +0100

    Lein 2 project.clj compatibility

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A HTTP route matching library"
   :url "http://github.com/weavejester/clout"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[ring-mock "0.1.1"]])
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.1"]]}})
