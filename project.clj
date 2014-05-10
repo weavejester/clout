57f1602493b55a923486a1b9c4a9d190eaebeafe
James Reeves <jreeves@weavejester.com>
2014-05-10 22:47:17 +0100

    Updated base Clojure dependency to 1.3.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clout "1.1.0"
   :description "A HTTP route matching library"
   :url "http://github.com/weavejester/clout"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
