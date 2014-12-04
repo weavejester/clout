ff931a14624214b26d150e071b51dcfd098c64fe
James Reeves <jreeves@weavejester.com>
2014-12-04 10:22:31 +0000

    Release 2.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clout "2.0.0"
+(defproject clout "2.1.0"
   :description "A HTTP route matching library"
   :url "https://github.com/weavejester/clout"
   :license {:name "Eclipse Public License"
