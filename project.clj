2205652b03220e79be343f30314dac7638ba1c8c
James Reeves <jreeves@weavejester.com>
2015-04-01 19:04:40 +0100

    Update Instaparse to 1.3.6 (fixes #24)

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.3.4" :exclusions [org.clojure/clojure]]]
+                 [instaparse "1.3.6" :exclusions [org.clojure/clojure]]]
   :profiles
   {:dev {:jvm-opts ^:replace []
          :dependencies [[ring-mock "0.1.5"]
